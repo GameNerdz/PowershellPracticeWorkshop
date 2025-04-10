ca0e719e21f6c0d0dc4bda91dcc2b3d24c76ea85
GameNerdz <joey.real.w@gmail.com>
2025-04-10 10:46:57 -0400

    Changelog: * Re-arranged the script so settings are set first, then create folders. * Removed Powershell version requirement as It wasnt needed. * Added the ability to create assignment folders in class/course and week folders depending on preference.

--- Scripts/New-SemesterFolder.ps1
+++ Scripts/New-SemesterFolder.ps1
@@ -2,12 +2,12 @@
 # Creates Folders for College Semester in OneDrive folders and Custom locations, Including Week Folders.
 # Joseph Wahba
 
-#Require -Version 7.5
+# Settings Phase
 
 # Check where the user wants the folder to be created
 $LocationCheck = Read-Host 'Where do you want to create the semester folder? Enter:
-"Home" for OneDrive Home root folder
-"Work" for OneDrive Commercial root folder
+"Home" for OneDrive home root folder
+"Work" for OneDrive commercial root folder
 Leave blank for a custom folder location.
 
 Input'
@@ -26,17 +26,12 @@ switch ($LocationCheck) {
         $PathLoc = $FolderBrowser.SelectedPath
     }
 }
-
+#Write Folder path
 Write-Host "The folder path is: $PathLoc"
-
-# Create Root Sememster Folder
-$Semester = Read-Host "What Semester are you creating folders for?"
-New-Item -Path "$PathLoc\" -name "Semester $Semester" -ItemType Directory
-Write-Output " Created: $Path\Semester $Semester\"
 
 # Check if User wants weekly folders
 do {
-    $WeekFolderCheck = Read-Host "Do you want Week folders? Y/N"
+    $WeekFolderCheck = Read-Host "Do you want week folders? Y/N"
     switch ($WeekFolderCheck) {
         Y {Write-Host "Continuing with creating week folders."}
         N {Write-Host "Skipping creating week folders."}
@@ -48,8 +43,7 @@ do {
 
 # If WeekfolderCheck is Y, Determine how many weeks a semester is.
 if ($WeekFolderCheck -eq "Y") {
-    # Check how many weeks the semester is
-        [Int]$Weeks = Read-Host "How many weeks is your Semester? (Leave Blank for Default: 14 Weeks)"
+        [Int]$Weeks = Read-Host "How many weeks is your semester? (Leave blank for default: 14 weeks)"
 
     if ($Weeks -eq 14 -xor $Weeks -eq 0 ) {
         $Weeks = 14
@@ -60,17 +54,57 @@ if ($WeekFolderCheck -eq "Y") {
     }
 }
 
-# Create Class Folders
+# Check if user wants assignment folders
 do {
-    $Class = Read-Host 'Enter "CourseCode - Course Name". "End" to end'
+    $AssignmentFolderCheck = Read-Host 'Do you want an Assignments folder to be created in each class/course folder or in each week folder?
+    "C" for the Assignments folder to be made in each Class/Course folder.
+    "W" for the Assignments folder to be made in each Week folder.
+    "N" to skip making an assignments folder all together.
+
+    Input'
+    switch ($AssignmentFolderCheck) {
+        C {Write-Host "Creating asignments folders in class/course folders."}
+        W {
+            if ($WeekFolderCheck -eq "Y") {
+                Write-Host "Creating asignments folders in week folders."
+            }
+            else {
+                Write-Host "Week Folders are not enabled! Please re-run script and enable week folders!"
+                Exit
+            }
+        }
+        N {Write-Host "Skipping creating assignments folders"}
+        Default {Write-Host "Invalid entry. Please enter again"}
+    }
+} until (
+    $AssignmentFolderCheck -eq 'C' -xor $AssignmentFolderCheck -eq 'W' -xor $AssignmentFolderCheck -eq 'N'
+)
+
+# Creation Phase
+$Semester = Read-Host "What semester are you creating folders for?"
+New-Item -Path "$PathLoc\" -name "Semester $Semester" -ItemType Directory
+Write-Output " Created: $Path\Semester $Semester\"
+
+do {
+    $Class = Read-Host 'Enter your class name or the course code. Enter "End" to end'
     if ($Class -ne "End") {
         New-Item -Path "$PathLoc\Semester $Semester\" -Name $Class -ItemType Directory
         Write-Output " Created: $Path\Semester $Semester\$Class"
         if ($WeekFolderCheck -eq "Y") {
             for ($i = 1; $i -le $Weeks; $i++) {
                 New-Item -Path "$PathLoc\Semester $Semester\$Class\" -Name "Week $i" -ItemType Directory
+                if ($AssignmentFolderCheck -eq "W") {
+                    New-Item -Path "$PathLoc\Semester $Semester\$Class\Week $i" -Name "Assignments" -ItemType Directory
+                }
             }
-            Write-Output "Created: Week Folders"
+            Write-Output "Created: Week folders"
+            if ($AssignmentFolderCheck -eq "W") {
+                Write-Output "Created Assignment folders in Week folders."
+            }
+        }
+        if ($AssignmentFolderCheck -eq "C") {
+            New-Item -Path "$PathLoc\Semester $Semester\$Class\" -Name "Assignments" -ItemType Directory
+            Write-Output "Created: Assignment folder"
         }
     }
 } until (
