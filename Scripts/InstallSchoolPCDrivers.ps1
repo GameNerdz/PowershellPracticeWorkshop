bff95c11131e1df7be56d90fb9f6d9b084af0efa
Joseph Wahba <Joey.real.w@gmail.com>
2025-08-11 07:29:11 -0400

    Fixed Script and Improved.
    Now finds exe files with "Setup" in the name or starts with "Dolby" and runs those installers.
    
    Added the ability to change the installers the script looks for by adding it to $InstallerTypes.
    
    Now requires administrator!

--- Scripts/InstallSchoolPCDrivers.ps1
+++ Scripts/InstallSchoolPCDrivers.ps1
@@ -1,6 +1,9 @@
 # InstallSchoolPCDrivers.ps1
 # Scripting my school PC's drivers install so I can be lazy and not have to do them all one by one manually. God I wish Gigabyte had a tool to do a driver install or their factory image tool (Gigabyte Smart USB Backup) worked.
 # Joseph Wahba
+#Requires -RunAsAdministrator
+
+$InstallerTypes = @("*Setup*.exe", "Dolby*.exe")
 
 Add-Type -AssemblyName System.Windows.Forms
 $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
@@ -10,68 +13,77 @@ $null = $FolderBrowser.ShowDialog()
 $PathLoc = $FolderBrowser.SelectedPath
 
 Write-Output "Begin Installing Drivers!"
-Write-Host "Drivers: Audio"
-
-$DriverDir = "$PathLoc\Audio\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
-}
-
 Write-Host "Drivers: Bluetooth"
 
-$DriverDir = "$PathLoc\Bluetooth\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\Bluetooth\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: Card Reader"
 
-$DriverDir = "$PathLoc\Card Reader\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\Card Reader\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: Chipset"
 
-$DriverDir = "$PathLoc\Chipset\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\Chipset\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: LAN"
 
-$DriverDir = "$PathLoc\LAN\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\LAN\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: Utility"
 
-$DriverDir = "$PathLoc\Utility\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\Utility\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: VGA"
 
-$DriverDir = "$PathLoc\VGA\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\VGA\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
 
 Write-Host "Drivers: WLAN"
 
-$DriverDir = "$PathLoc\WLAN\"
-
-foreach ($File in $DriverDir) {
-    Start-Process -FilePath "$DriverDir\$File" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+$DriverFolders = Get-ChildItem -Path "$PathLoc\WLAN\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
 }
+
+Write-Host "Drivers: Audio"
+
+$DriverFolders = Get-ChildItem -Path "$PathLoc\Audio\" -Directory | Select-Object -ExpandProperty FullName
+foreach ($Folder in $DriverFolders) {
+    $SetupFile = Get-ChildItem -Path "$Folder/*" -File -Include $InstallerTypes
+    Write-Output "Installing $($SetupFile.FullName)"
+    Start-Process -FilePath "$($SetupFile.FullName)" -ArgumentList "/s", "/S", "/silent", "/SILENT", "-silent", "-SILENT", "/q", "/Q", "/qn", "/QN", "/quiet", "/QUIET", "-unattended", "-UNATTENDED", "/passive", "/PASSIVE", "/norestart", "/NORESTART" -Wait -NoNewWindow
+}
+
 
 Write-Output "Installing Drivers Complete!"
